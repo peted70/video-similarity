1d24bab5caa82738dfc0819a84b8b90064fe2e29
Pete D <peted70@hotmail.com>
2021-02-26 15:13:48 +0000

    added poject files

---
+++ index.js
@@ -0,0 +1,91 @@
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+const ffprobe = require('@ffprobe-installer/ffprobe');
+
+let optObj = {};
+let hashBits = optObj.hashBits || 8;
+let strength = optObj.strength || 1;
+
+const vHash = require('video-hash')({
+    ffmpegPath: ffmpeg.path,
+    ffprobePath: ffprobe.path,
+    hashBits: hashBits,
+    strength: strength
+});
+
+const {
+    performance
+} = require('perf_hooks');
+  
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const fetch = require("node-fetch");
+
+const unlink = util.promisify(fs.unlink);
+
+async function hashVideo(videoPath) {
+    const video = vHash.video(videoPath);
+    try {
+        let hash = await video.hash();
+        return hash;
+    } catch (err) {
+        throw err;
+    }
+}
+
+function streamFile(res, filename) {
+    return new Promise((success, error) => {
+        const dest = fs.createWriteStream(filename);
+        res.body.pipe(dest);
+        res.body.on('end', () => success());
+        dest.on('error', error);
+    });
+}
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    /*
+        {
+            videourl: 'https://contentsimilaritystore.blob.core.windows.net/input/Big_Buck_Bunny_1080_10s_30MB.mp4',
+            hashbits: 8,
+            strength: 1
+        }
+    */
+    let startTime = performance.now();
+
+    /** First, we need to download the file locally as we can't stream to the video hash service */
+    let response = await fetch(req.body.videourl);
+  
+    /** Create a unique-ish filename */
+    let localFilename = new Date().toISOString().replace(/[-:.]/g,"") + path.extname(req.body.videourl);
+
+    // This works but will load the whole video into memory
+    // let buff = await response.buffer();
+    // fs.writeFileSync(localFilename, buff, "binary");
+
+    // Instead stream the file to disk 
+    await streamFile(response, localFilename);
+    console.log(`created ${localFilename}`);        
+
+    let hash = '';
+    try {
+        /** Initialize the hasbits and strength parameters */
+        vHash.options.hashBits = req.body.hashbits || hashBits;
+        vHash.options.strength = req.body.srength || strength;
+
+        /** It is possible to pass a stream here but we hit an error in ffmpeg so will use a local file */
+        hash = await hashVideo(localFilename)
+    } finally {
+        await unlink(localFilename);
+        console.log(`deleted ${localFilename}`);        
+
+        let processingTime = performance.now() - startTime;
+        console.log(`processed ${req.body.videourl} in ${new Date(processingTime).toISOString().slice(11, -1)}`);
+    }
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: hash
+    };
+}
+
